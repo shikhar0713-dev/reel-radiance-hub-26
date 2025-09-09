ec1f40a6720e13ee06afe5c1973c0030fff88061
shikhar0713-dev <shikhar18.123@gmail.com>
2025-09-09 16:14:01 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
+// Clerk components
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -13,10 +16,23 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+
+      {/* Example header with Clerk */}
+      <header className="p-4 border-b flex justify-between">
+        <h1 className="text-xl font-bold">Reel Radiance Hub</h1>
+        <div>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </header>
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
